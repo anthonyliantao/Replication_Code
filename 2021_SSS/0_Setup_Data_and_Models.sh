0e66137cb9381bce639732db868a67f8f67e6bb6
Antao Li <37994491+anthonyliantao@users.noreply.github.com>
2025-07-07 07:24:05 +0800

    refract: 删除其他复现库

--- 2021_SSS/0_Setup_Data_and_Models.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript --vanilla --verbose 0_Setup_Data_and_Models.R > logs/0_Setup_Data_and_Models.log 2>&1
--- 2021_SSS/6_Analyze_Results.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript --vanilla --verbose 6_Analyze_Results.R > logs/6_Analyze_Results.log 2>&1
